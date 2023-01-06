4d241bacfc237875edfd7b5a08d62966de2537e1
vicky-mac <113899556+vicky-mac@users.noreply.github.com>
2023-01-06 07:49:55 +0000

    Deploying to gh-pages from  @ 3135e4cc2bdc560306c8e2264081c17fcc70fd1d 🚀

---
+++ precache-manifest.3d0b2e84ee806ef22761c3783cd7dea4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6003417ab080e9396a95e545a1dab65d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d839a6185ab73a0fe6b8",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "b0eb437ef53a24e4331c",
+    "url": "/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "3a8042aed5b01a6850f4",
+    "url": "/static/css/main.52a26c30.chunk.css"
+  },
+  {
+    "revision": "2de50b0640f133b9e361",
+    "url": "/static/js/2.1b435803.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/static/js/2.1b435803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d839a6185ab73a0fe6b8",
+    "url": "/static/js/3.4b294d54.chunk.js"
+  },
+  {
+    "revision": "b0eb437ef53a24e4331c",
+    "url": "/static/js/4.4b501e37.chunk.js"
+  },
+  {
+    "revision": "3a8042aed5b01a6850f4",
+    "url": "/static/js/main.001f1299.chunk.js"
+  },
+  {
+    "revision": "80e2597530d8ace10e4b",
+    "url": "/static/js/runtime-main.7eb266cb.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "667d5747212969eda7dd02bdf3bf175e",
+    "url": "/static/media/Netflix_logo.667d5747.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "2f5620d089f5a9abf59414f8cede432d",
+    "url": "/static/media/pristUniversity.2f5620d0.png"
+  },
+  {
+    "revision": "5cf2802bdb87e2a39978583d0d64d809",
+    "url": "/static/media/redblox.5cf2802b.jpeg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "4ffd84571eac907f0e8c03ecdf02a606",
+    "url": "/static/media/tesla_logo.4ffd8457.jpeg"
+  }
+]);
